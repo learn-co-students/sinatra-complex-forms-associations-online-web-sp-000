5263e8ede083dcba096366981382651cbaa52149
aburk3 <aburk3@gmail.com>
2018-12-14 20:39:22 -0600

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -34,6 +34,16 @@ class PetsController < ApplicationController
    redirect "pets/#{@pet.id}"
  end
 
+ patch '/pets/:id' do
+   @pet = Pet.find(params[:id])
+   @pet.update(params["pet"])
+   if !params["owner"]["name"].empty?
+     @pet.owner = Owner.create(name: params["owner"]["name"])
+   end
+   @pet.save
+   redirect "pets/#{@pet.id}"
+ end
+
  get '/pets/:id/edit' do
    @pet = Pet.find(params[:id])
    erb :'/pets/edit'
