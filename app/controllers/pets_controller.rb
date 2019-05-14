c0e1fd32bfd79dc5bd7ab469644a3efd99f90e47
Jeff Benton <sikletz@gmail.com>
2019-05-14 19:01:00 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,19 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    if params[:owner_id]
+      @pet = Pet.create(name: params[:pet_name], owner_id: params[:owner_id])
+    else
+      @pet = Pet.create(name: params[:pet_name])
+      @owner = Owner.create(name: params[:owner_name])
+      @pet.owner = @owner
+      @owner.pets << @pet
+    end
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +27,20 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
+  patch '/pets/:id' do
+    if params[:owner_name].empty?
+      @pet = Pet.update(params[:id], name: params[:pet_name], owner_id: params[:owner_id])
+    else
+      @owner = Owner.create(name: params[:owner_name])
+      @pet = Pet.update(params[:id], name: params[:pet_name], owner_id: @owner.id)
+      @owner.pets << @pet
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
