3de8199e94f1567a202fe4fce0c84e56dabb8494
Ramon Chavez <rachavez1974@gmail.com>
2019-07-20 01:50:59 -0400

    Add create functionality

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,12 +5,18 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all 
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    @owner = Owner.create(name: params[:owner_name])
+    @pet = Pet.create(name: params[:pet_name], owner_id: @owner.id)
 
+    if !params[:owner_id].empty?
+      @pet = Pet.create(pet_params(params))
+    end
     redirect to "pets/#{@pet.id}"
   end
 
@@ -20,7 +26,14 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-
     redirect to "pets/#{@pet.id}"
   end
+
+  private
+  def pet_params(params)
+    {  :name => params[:pet_name],
+       :owner_id => params[:owner_id] 
+    }
+  end
+
 end
