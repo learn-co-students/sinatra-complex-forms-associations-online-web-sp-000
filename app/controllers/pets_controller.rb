51479f697372e8aa7d6bdccc673171e8a2c1abe8
arielord <arielord@buffalo.edu>
2019-08-11 02:58:09 -0400

    All test passing

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,49 +2,53 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
     #binding.pry
     @pet = Pet.create(params[:pet])
-    
+
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
       @pet.save
     end
-    
+
     redirect to "pets/#{@pet.id}"
   end
-  
-  get '/pets/:id/edit' do 
+
+  get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     @owners = Owner.all
     erb :'/pets/edit'
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  patch '/pets/:id' do
     @pet = Pet.find(params[:id])
-    
+
     if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
       @pet.save
+    else
+      if !params["pet"]["owner_id"].empty?
+        @pet.owner = Owner.find(params["pet"]["owner_id"])
+        @pet.save
+      end
     end
-    
+
     @pet.name = params["pet"]["name"]
-    @pet.owner = Owner.find(id: params["pet"]["owner_id"])
     @pet.save
-    
+
     redirect to "pets/#{@pet.id}"
   end
 end
