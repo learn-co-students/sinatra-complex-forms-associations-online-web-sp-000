3f7ba4f03177a0519bcaba4503040715d5c44ff5
Alice Brunel <alc.brunel@gmail.com>
2019-10-01 12:27:54 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,50 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+    
+    @pet = Pet.create(params[:pet])
+      if !params["owner"]["name"].empty?
+        @pet.owner = Owner.create(name: params["owner"]["name"])
+        
+      end
+      
+      # Manually save because no shovel operator
+      @pet.save
+      
+    redirect "/pets/#{@pet.id}"
   end
+
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
 
-    redirect to "pets/#{@pet.id}"
+  get '/pets/:id/edit' do 
+    @owners = Owner.all 
+    @pet = Pet.find(params[:id])
+    
+    erb :'/pets/edit'
+  end
+
+  patch '/pets/:id' do 
+    
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    # Remove all previous owner    
+    
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    # Manually save because no shovel operator
+    @pet.save
+    
+    redirect "pets/#{@pet.id}"
   end
 end
