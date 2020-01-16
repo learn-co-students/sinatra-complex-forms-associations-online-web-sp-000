3d1559b0068d1eced8dd6eff90689f726215487c
RickLearnsToCode <rick.rosetti@gmail.com>
2020-01-15 19:03:32 -0700

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,27 +2,58 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
-    @owners = Owner.all 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    # binding.pry
     @pet = Pet.create(params[:pet])
+    # binding.pry
+    if !Owner.find_by_id(params[:owner][:id])
+      @owner = Owner.new(params[:owner])
+      @pet.owner = @owner
+      @pet.save
+    else
+      @pet.owner = Owner.find_by_id(params[:owner][:id])
+      @pet.save
+    end
 
-    @pet.owner = Owner.find_by_id(owner[:id])
-    redirect to "pets/#{@pet.id}"
+    redirect to "/pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    # binding.pry
+    erb :'/pets/edit'
+
+  end
+
+  patch '/pets/:id' do
+    # binding.pry
+    @pet = Pet.find(params[:id])
+    if !!params[:pet][:name]
+      @pet.name = params[:pet][:name]
+    end
+    # binding.pry
+    if !!Owner.find_by_id(params[:pet][:owner_id]) && params[:owner][:name].empty?
+      @pet.owner = Owner.find_by_id(params[:pet][:owner_id])
+      @pet.save
+    else
+      @owner = Owner.new(params[:owner])
+      @pet.owner = @owner
+      @pet.save
+    end
 
     redirect to "pets/#{@pet.id}"
   end
