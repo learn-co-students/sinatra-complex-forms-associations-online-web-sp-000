b905f01a8c4d102b8fd086f7123515a345f26bbe
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-02-01 19:50:28 +0000

    final code with comments

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,9 @@
 class Owner < ActiveRecord::Base
   has_many :pets
 end
+=begin
+Active Record is smart enough to take that key of pet_ids,
+ pointing to an array of numbers, find the pets that have those IDs,
+  and associate them to the given owner, all because we set up
+   our associations such that an owner has many pets.
+=end
