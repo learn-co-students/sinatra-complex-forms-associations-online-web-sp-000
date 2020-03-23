5aa2555b47e4fe5c88ead5ff3cdf12bdfb42873c
CoderJay06 <jaycru06@yahoo.com>
2020-03-23 17:36:43 -0400

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -16,7 +16,7 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    @owner.save 
+   
 
     redirect "owners/#{@owner.id}"
   end
@@ -34,9 +34,9 @@ class OwnersController < ApplicationController
 
   patch '/owners/:id' do
     # ####### bug fix
-    # if !params[:owner].keys.include?("pet_ids")
-    #   params[:owner]["pet_ids"] = []
-    # end
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end
     # #######
  
     @owner = Owner.find(params[:id])
