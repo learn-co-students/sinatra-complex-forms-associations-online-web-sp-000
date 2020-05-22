6f137da6607ca231dc5bb58386a4ece7f33f9bb0
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2020-05-21 20:18:16 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,10 +6,12 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    @pet = Pet.creat(params[:pet])
 
     redirect to "pets/#{@pet.id}"
   end
