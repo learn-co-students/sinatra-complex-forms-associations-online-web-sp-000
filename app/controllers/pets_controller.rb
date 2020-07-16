b8f0b147f2527f665c800e63cce4f141afb4a86d
AustinRhoads <austinrrhoads@ymail.com>
2020-07-15 22:52:35 -0500

    all tests pass

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,12 +6,24 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
 
-    redirect to "pets/#{@pet.id}"
+     ####### bug fix
+     if !params[:pet].keys.include?("owner_id")
+       params[:pet]["owner_id"] = []
+     end
+       #######
+    if !params[:owner][:name].empty?
+     @owner = Owner.create(params[:owner])
+     params[:pet]["owner_id"] = @owner.id
+    end
+ 
+     @pet = Pet.create(params[:pet])
+     redirect to "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
@@ -19,7 +31,23 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'pets/edit'
+  end
+
+
   patch '/pets/:id' do 
+    if !params[:owner][:name].empty?
+      @owner = Owner.create(name: params[:owner][:name])
+      params[:pet][:owner_id] = @owner.id
+    end
+
+    @pet = Pet.find(params[:id])
+    
+    @pet.update(params[:pet])
+    #binding.pry
 
     redirect to "pets/#{@pet.id}"
   end
