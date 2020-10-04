d44db4ed8426c9eff902dc87a0a9496359fa06c6
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-10-04 03:19:53 -0700

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,15 +6,33 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do 
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do 
+    # create new owner AND associate pets via pet_ids
+    if params[:owner][:name].empty?
+      return erb :'/owners/failures/name'
+    else
+      @owner = Owner.create(params[:owner])
+    end
+
+    # if applicable, add new pet
+    if !params[:pet][:name].empty? && !params[:owner][:name].empty?
+      @owner.pets.create(params[:pet])
+    end
+    # alternative:
+    # if !params["pet"]["name"].empty?
+    #   @owner.pets << Pet.create(name: params["pet"]["name"])
+    # end
     
+    redirect "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
@@ -24,6 +42,21 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do 
-   
+    ####### bug fix ############# NOTE: This is required so that it's possible to remove ALL previous pets from owner.
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end
+    #######
+
+    @owner = Owner.find(params[:id])
+    @owner.update(params[:owner])
+    if !params[:pet][:name].empty?
+      @owner.pets.create(params[:pet])
+    end
+    # can also use below:
+    # if !params["pet"]["name"].empty?
+    #   @owner.pets << Pet.create(name: params["pet"]["name"])
+    # end
+    redirect "owners/#{@owner.id}"
   end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,12 +6,25 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    check_for_errors
 
-    redirect to "pets/#{@pet.id}"
+    #decide whether to create an owner
+    if !params[:owner][:name].empty?
+      owner = Owner.create(params[:owner])
+    else
+      owner = Owner.find(params[:pet][:owner_ids].first)      
+    end
+
+    #create pet
+    owner.pets.create(name: params[:pet][:name], owner: owner)
+
+    #redirect
+    redirect to "pets/#{owner.pets.last.id}"
   end
 
   get '/pets/:id' do 
@@ -19,8 +32,49 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+
   patch '/pets/:id' do 
 
-    redirect to "pets/#{@pet.id}"
+    #check for errors first
+    check_for_errors
+
+    #decide whether to create an owner  
+    if !params[:owner][:name].empty?
+      new_owner = Owner.create(params[:owner])
+    elsif
+      new_owner = Owner.find(params[:pet][:owner_ids].first)
+    end
+
+    #update pet
+    pet = Pet.find(params[:id])
+    pet.update(name: params[:pet][:name], owner: new_owner)
+
+    #redirect
+    redirect to "pets/#{pet.id}"
   end
+
+  #helpers
+  def check_for_errors
+
+    ## BUG FIX
+    if !params[:pet].keys.include?("owner_ids")
+      params[:pet]["owner_ids"] = []
+    end
+    ##
+
+    #show error if no pet name
+    if params[:pet][:name].empty?
+      return erb :'/pets/failures/name'
+    #show error if > 1 owner selected
+    elsif params[:pet][:owner_ids].size > 1
+      return erb :'/pets/failures/multiple_owners'
+    end   
+
+  end
+
 end
