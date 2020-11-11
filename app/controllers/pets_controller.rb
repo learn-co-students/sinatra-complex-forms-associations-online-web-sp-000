d8dc600501a4f248cf3507612c625ed7e5d135b3
jeanettepayne <jeanetterpayne@gmail.com>
2020-11-10 20:27:24 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,12 +6,26 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    # binding.pry
+    @pet = Pet.create(name: params["pet"]["name"])
+    if !params["pet"]["owner_id"].empty?
+      @pet.owner = Owner.find_by(id: params["pet"]["owner_id"])
+    elsif params["pet"]["owner_id"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
   end
 
   get '/pets/:id' do 
@@ -20,7 +34,9 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    
     redirect to "pets/#{@pet.id}"
   end
 end
