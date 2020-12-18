d51b3504d84009553754700fa4cd470b71910bd8
asb1512 <andrew@olympicmedia.com>
2020-12-18 14:39:55 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,13 +5,24 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(name: params[:pet][:name], owner_id: params[:pet][:owner_id])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'pets/edit'
   end
 
   get '/pets/:id' do 
@@ -19,8 +30,18 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.name = params[:pet][:name]
 
+    if params[:owner][:name].empty?
+      @owner = Owner.find(params[:pet][:owner_id])
+      @pet.owner = @owner
+    else
+       @owner = Owner.create(name: params[:owner][:name])
+       @pet.owner = @owner
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
