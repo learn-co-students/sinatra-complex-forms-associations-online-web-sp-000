93599240a950f8f8472acc5285402b186d7ee37d
catprice444 <catprice444@gmail.com>
2020-12-21 14:44:20 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,22 +5,34 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+   post '/pets' do     
+    @pet = Pet.create(params[:pet])
+    
+   if !params[:owner][:name].empty?
+    @pet.owner = Owner.create(name: params[:owner][:name])
+   end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
-    @pet = Pet.find(params[:id])
-    erb :'/pets/show'
-  end
+    post '/pets/:id' do 
+      @pet = Pet.find(params[:id])
+      @pet.update(params[:pet])
+      if !params[:owner][:name].empty?
+        @pet.owner = Owner.create(name: params[:owner][:name])
+      end
+        @pet.save
+        redirect to "pets/#{@pet.id}"
+    end
+  
 
-  patch '/pets/:id' do 
-
-    redirect to "pets/#{@pet.id}"
+  get '/pets/:id/edit' do
+   @pet = Pet.find(params[:id])
+   erb:'/pets/edit'
   end
+  
 end
