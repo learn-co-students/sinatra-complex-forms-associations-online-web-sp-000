342f33ac0179dc3b48f87cf2c4074d6085515585
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-02-06 11:58:14 +0000

    Done.

--- db/migrate/02_create_pets_table.rb
+++ db/migrate/02_create_pets_table.rb
@@ -1,8 +1,8 @@
 class CreatePetsTable < ActiveRecord::Migration
   def change
     create_table :pets do |t|
-      t.string :name
-      t.integer :owner_id
+      t.string   :name
+      t.integer  :owner_id
     end
   end
 end
