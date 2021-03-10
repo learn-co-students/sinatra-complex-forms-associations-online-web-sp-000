aa2d16160f0e1d11d9ee4a2e922252ac2d186975
jasonronalddavis <jdavis@c.ringling.edu>
2021-03-10 08:29:11 -0600

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -18,11 +18,12 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do
+    binding.pry
     @owner = Owner.create(params[:owner])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    redirect "owners/#{@owner.id}"
+    redirect "/owners/#{@owner.id}"
   end
 
 
@@ -45,6 +46,6 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    redirect "owners/#{@owner.id}"
+    redirect "/owners/#{@owner.id}"
 end
 end
