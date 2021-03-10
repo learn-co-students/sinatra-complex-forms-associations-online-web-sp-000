67de2002736266890b066efe775e32784227fa55
jasonronalddavis <jdavis@c.ringling.edu>
2021-03-10 08:01:45 -0600

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,17 +1,33 @@
 class PetsController < ApplicationController
 
+
+
+  configure do
+    enable :sessions
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "auth_demo_lv"
+  end
+
   get '/pets' do
     @pets = Pet.all
     erb :'/pets/index' 
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
+
   end
 
   post '/pets' do 
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      owner = Owner.create(name: params["owner"]["name"])
+      owner.pets << @pet
+    end
+    redirect "pets/#{@pet.id}"
 
-    redirect to "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
@@ -19,8 +35,18 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
 
-    redirect to "pets/#{@pet.id}"
+  patch '/pets/:id' do 
+    if !params[:pet].keys.include?("owner_id")
+      params[:pet]["owner_id"] = []
+      end
+      @pet = Pet.find(params[:id])
+      @pet.update(params["pet"]) 
+      if !params["owner"]["name"].empty?
+        owner = Owner.create(name: params["owner"]["name"])
+        owner.pets << @pet
+      end
+  redirect "pets/#{@pet.id}"
+
   end
 end
